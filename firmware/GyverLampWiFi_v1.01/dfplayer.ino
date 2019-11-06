ec4d8c4c2b3a036c8222af7e06da820cc7d3f757
AndyIP70 <54570953+AndyIP70@users.noreply.github.com>
2019-11-06 23:20:59 +0700

    Update dfplayer.ino

--- firmware/GyverLampWiFi_v1.01/dfplayer.ino
+++ firmware/GyverLampWiFi_v1.01/dfplayer.ino
@@ -1,5 +1,12 @@
 void InitializeDfPlayer1() {
+#if defined(ESP8266)
   mp3Serial.begin(9600);  
+#endif
+
+#if defined(ESP32)
+  mp3Serial.begin(9600, SRX, STX);  
+#endif
+
   dfPlayer.begin(mp3Serial, true, true);
   dfPlayer.setTimeOut(2000);
   dfPlayer.EQ(DFPLAYER_EQ_NORMAL);

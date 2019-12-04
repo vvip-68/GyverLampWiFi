69f175eaedd0855f99082edc7bbe31d8a59bef95
vvip <vvip@mail.ru>
2019-12-04 13:33:40 +0700

    Испралено - кнопка "Предыдущий" переключала назад только до первого эффекта в списке

--- firmware/GyverLampWiFi_v1.02/custom.ino
+++ firmware/GyverLampWiFi_v1.02/custom.ino
@@ -186,16 +186,16 @@ void prevModeHandler() {
 
   while (aCnt < MAX_EFFECT) {
     // Берем предыдущий режим по циклу режимов
-    aCnt++; thisMode--; // thisMode: byte => при переполнении вернет 255 
-    if (thisMode >= MAX_EFFECT) thisMode = MAX_EFFECT - 1;
+    aCnt++; thisMode--; 
+    if (thisMode < 0) thisMode = MAX_EFFECT - 1;
 
     // Если новый режим отмечен флагом "использовать" - используем его, иначе берем следующий (и проверяем его)
     if (getEffectUsage(thisMode)) break;
     
-    // Если перебрали все и ни у адного нет флага "использовать" - не обращаем внимание на флаг, используем следующий
+    // Если перебрали все и ни у одного нет флага "использовать" - не обращаем внимание на флаг, используем предыдцщий
     if (aCnt >= MAX_EFFECT) {
-      thisMode = curMode--; // thisMode: byte => при переполнении вернет 255
-      if (thisMode >= MAX_EFFECT) thisMode = MAX_EFFECT - 1;
+      thisMode = curMode--;
+      if (thisMode < 0) thisMode = MAX_EFFECT - 1;
       break;
     }
   }
